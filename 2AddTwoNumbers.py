dd7a017a5b9e62c5ce1448b2c5e6a9816fe4f1b3
ansi7794 <anushree@bu.edu>
2018-10-01 12:11:40 -0400

    Update 2AddTwoNumbers.py

--- 2AddTwoNumbers.py
+++ 2AddTwoNumbers.py
@@ -28,3 +28,24 @@ class Solution:
             carry //= 10
         
         return head.next
+
+def compareLinkedList(l1, l2):
+    while l1 or l2:
+        if not (l1 and l2) or l1.val != l2.val:
+            return False
+        l1 = l1.next
+        l2 = l2.next
+    return True
+
+
+if __name__ == "__main__":
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
+    lsum = ListNode(7)
+    lsum.next = ListNode(0)
+    lsum.next.next = ListNode(8)
+    print(compareLinkedList(Solution().addTwoNumbers(l1, l2), lsum))
